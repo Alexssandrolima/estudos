31028e5a723f709df6569da66e554a5bbfadeb74
alexssandrolima I3 <alexssandrolima@gmail.com>
2015-10-06 16:53:21 -0300

    Criado no projeto, imprimindo diretamente para a impressora,tipo: comitando um novo projeto

---
+++ ImprimindoTextoDiretoImpressora/FormImpressao.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ImprimindoTextoDiretoImpressora
+{
+    partial class FormImpressao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxImpressoras = new System.Windows.Forms.ComboBox();
+            this.buttonImprimir = new System.Windows.Forms.Button();
+            this.textBoxImpressao = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxImpressoras
+            // 
+            this.comboBoxImpressoras.FormattingEnabled = true;
+            this.comboBoxImpressoras.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxImpressoras.Name = "comboBoxImpressoras";
+            this.comboBoxImpressoras.Size = new System.Drawing.Size(459, 21);
+            this.comboBoxImpressoras.TabIndex = 0;
+            // 
+            // buttonImprimir
+            // 
+            this.buttonImprimir.Location = new System.Drawing.Point(477, 10);
+            this.buttonImprimir.Name = "buttonImprimir";
+            this.buttonImprimir.Size = new System.Drawing.Size(75, 23);
+            this.buttonImprimir.TabIndex = 1;
+            this.buttonImprimir.Text = "&Imprimir";
+            this.buttonImprimir.UseVisualStyleBackColor = true;
+            this.buttonImprimir.Click += new System.EventHandler(this.buttonImprimir_Click);
+            // 
+            // textBoxImpressao
+            // 
+            this.textBoxImpressao.Location = new System.Drawing.Point(12, 39);
+            this.textBoxImpressao.Multiline = true;
+            this.textBoxImpressao.Name = "textBoxImpressao";
+            this.textBoxImpressao.Size = new System.Drawing.Size(540, 344);
+            this.textBoxImpressao.TabIndex = 2;
+            // 
+            // FormImpressao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 395);
+            this.Controls.Add(this.textBoxImpressao);
+            this.Controls.Add(this.buttonImprimir);
+            this.Controls.Add(this.comboBoxImpressoras);
+            this.Name = "FormImpressao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Impressão de Texto Diretamente na Impressora";
+            this.Load += new System.EventHandler(this.FormImpressao_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxImpressoras;
+        private System.Windows.Forms.Button buttonImprimir;
+        private System.Windows.Forms.TextBox textBoxImpressao;
+    }
+}
+
---
+++ ImprimindoTextoDiretoImpressora/FormImpressao.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImprimindoTextoDiretoImpressora
+{
+    public partial class FormImpressao : Form
+    {
+        public FormImpressao()
+        {
+            InitializeComponent();
+            CarregarListaDeImpressoras();
+        }
+
+        private void CarregarListaDeImpressoras()
+        {
+            comboBoxImpressoras.Items.Clear();
+
+            foreach (var printer in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                comboBoxImpressoras.Items.Add(printer);
+            }
+        }
+
+        private void FormImpressao_Load(object sender, EventArgs e)
+        {
+            //this.FormBorderStyle = FormBorderStyle.None;  //tirar as bordas.
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            // Remove the control box so the form will only display client area.
+            //this.ControlBox = false;
+
+
+
+        }
+
+        private void buttonImprimir_Click(object sender, EventArgs e)
+        {
+            //using (var printDocument = new System.Drawing.Printing.PrintDocument())
+            //{
+            //    printDocument.PrintPage += printDocument_PrintPage;
+            //    printDocument.PrinterSettings.PrinterName = comboBoxImpressoras.SelectedItem.ToString();
+            //    printDocument.Print();
+            //}
+
+
+            using (var printDocument = new System.Drawing.Printing.PrintDocument())
+            {
+                printDocument.PrintPage += printDocument_PrintPage;
+                printDocument.PrinterSettings.PrinterName = comboBoxImpressoras.SelectedItem.ToString();
+                _texto = textBoxImpressao.Text;
+                printDocument.Print();
+            }
+
+        }
+
+        //imprimir texto
+        //void printDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        //{
+        //    var printDocument = sender as System.Drawing.Printing.PrintDocument;
+
+        //    if (printDocument != null)
+        //    {
+        //        e.Graphics.DrawImage(Image.FromFile("logo.png"), new Point(5, 5));
+        //    }
+        //}
+
+
+
+        //void printDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        //{
+        //    var printDocument = sender as System.Drawing.Printing.PrintDocument;
+
+        //    if (printDocument != null)
+        //    {
+        //        using (var font = new Font("Times New Roman", 14))
+        //        using (var brush = new SolidBrush(Color.Black))
+        //        {
+        //            e.Graphics.DrawString(
+        //                textBoxImpressao.Text,
+        //                font,
+        //                brush,
+        //                new RectangleF(0, 0, printDocument.DefaultPageSettings.PrintableArea.Width, printDocument.DefaultPageSettings.PrintableArea.Height));
+        //        }
+        //    }
+        //}
+
+
+        void printDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            var printDocument = sender as System.Drawing.Printing.PrintDocument;
+
+            if (printDocument != null)
+            {
+                using (var fonte = new Font("Times New Roman", 14))
+                using (var brush = new SolidBrush(Color.Black))
+                {
+                    int caracteresNaPagina = 0;
+                    int linhasPorPagina = 0;
+
+                    e.Graphics.MeasureString(
+                        _texto, fonte, e.MarginBounds.Size, StringFormat.GenericTypographic,
+                        out caracteresNaPagina, out linhasPorPagina);
+
+                    e.Graphics.DrawString(
+                        _texto.Substring(0, caracteresNaPagina),
+                        fonte,
+                        brush,
+                        e.MarginBounds);
+
+                    _texto = _texto.Substring(caracteresNaPagina);
+
+                    e.HasMorePages = _texto.Length > 0;
+                }
+            }
+        }
+
+
+        public string _texto { get; set; }
+    }
+}
